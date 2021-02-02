7da086992d40ddc7be0db893c873921bf6f38b38
NoaHadad <34161861+NoaHadad@users.noreply.github.com>
2021-02-02 09:45:18 +0200

    Update Pi.h

--- simple/src/Pi.h
+++ simple/src/Pi.h
@@ -25,7 +25,7 @@ public:
        block* RBF_sender;
        std::vector<std::array<block, 2>>* strings;
        block seed_sub_group;	   
-       //int connection_status_sender;
+       vector<int>* arr_indexes;
 	   
        //receiver
        struct receiver* receivers;
@@ -43,7 +43,6 @@ public:
        std::vector<int> intersection_recv;
        std::vector <int>& items; 
        Bitstring* BF; 
-       //int connection_status_recv;
 	   
        int Not;
        int Nbf;
@@ -56,45 +55,50 @@ public:
 
        Pi(std::vector <int>& items,int  items_size,int Not,int Nbf,int Nc,int k,int maxOnes,int seeds_num,int numOfOnes,uint32_t m_nSecParam, uint8_t* constSeed ,int parties,int ** ports, vector<string>& ips,int player);
       
-       crypto* getCrypto(); 	
+       crypto* getCrypto(); 
+	
        void init_sender(int port);
        static void init_sender(struct sender* s, int port);
        void init_receiver(const char* ip,int port);
        static void init_receiver(struct receiver* recv,const char* ip,int port);
 	
        void set_sub_group();
-       void send_sub_group();
+       unsigned long send_sub_group();
 
-       void recv_R();
+       int check_if_to_abort();
        int check_if_to_abort1();
-
-       void recv_func();
        int check_if_to_abort2();
+	
        void create_BF(std::set<int>* h_kokhav,unsigned int* hash_seeds, block seed);
+       void create_BF_threads(std::set<int>* h_kokhav,unsigned int* hash_seeds, block seed);
        void create_RBF_sender(synchGBF* r);
 	   
-       void write_as_a_receiver(void* msg,unsigned long size);
-       int read_as_a_receiver(void* msg,unsigned long size); 
-       void write_as_a_sender(void* msg,unsigned long size);
-       int read_as_a_sender(void* msg,unsigned long size); 
+       unsigned long write_as_a_receiver(void* msg,unsigned long size);
+       unsigned long read_as_a_receiver(void* msg,unsigned long size); 
+       unsigned long write_as_a_sender(void* msg,unsigned long size);
+       unsigned long read_as_a_sender(void* msg,unsigned long size); 
 	   
-       static void write_as_a_sender(sender* snd,void* msg,unsigned long size);
-       static void write_as_a_receiver(receiver* recv,void* msg,unsigned long size);
-       static int read_as_a_receiver(receiver* recv,void* msg,unsigned long size);
-       static int read_as_a_sender(sender* snd,void* msg,unsigned long size);
+       static unsigned long write_as_a_sender(sender* snd,void* msg,unsigned long size);
+       static unsigned long write_as_a_receiver(receiver* recv,void* msg,unsigned long size);
+       static unsigned long read_as_a_receiver(receiver* recv,void* msg,unsigned long size);
+       static unsigned long read_as_a_sender(sender* snd,void* msg,unsigned long size);
 	   
-       void write_to_player(int other_player,void* msg,unsigned long size);
-       void read_from_player(int other_player,void* msg,unsigned long size);
-	   
-       void recv_C();
-       int check_if_to_abort();
+       unsigned long write_to_player(int other_player,void* msg,unsigned long size);
+       unsigned long read_from_player(int other_player,void* msg,unsigned long size);
+
+       unsigned long send_func();
+       unsigned long recv_func();
+       unsigned long recv_C();
 
        void compute_R_r();
-       void send_R_r();
+       unsigned long send_R_r();
+       void recv_R();
+	
+       void get_zeros_ones();	
        void arrange_the_indexes();
+	
        void create_RBF_receiver(synchGBF* r);
 
-       void send_func();
        ~Pi();
        int getNot() const;
        BitVector* getChoices() const;
