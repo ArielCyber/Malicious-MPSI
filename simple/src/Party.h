2badd18570422702766c0d77d68fa71ea01cacbb
NoaHadad <34161861+NoaHadad@users.noreply.github.com>
2021-02-08 11:35:23 +0200

    Update Party.h

--- simple/src/Party.h
+++ simple/src/Party.h
@@ -8,45 +8,111 @@ using namespace osuCrypto;
 
 class Party
 {
-public:
-
-       virtual crypto* getCrypto()=0; 
-       virtual void init_sender(int port)=0; 
-       virtual void init_receiver(const char* ip,int port)=0; 
-	   
-       virtual unsigned long send_sub_group()=0;  
-       virtual void set_sub_group()=0;  
-       virtual unsigned long recv_C()=0; 
-	   
-       virtual void compute_R_r()=0;
-       virtual unsigned long send_R_r()=0;
-       virtual unsigned long recv_R()=0;  
-	   
-       virtual void create_RBF_sender(synchGBF* r)=0; 
-       virtual void create_RBF_receiver(synchGBF* r)=0;	      
-	   
-       virtual unsigned long recv_func()=0;
-       virtual unsigned long send_func()=0; 
 	
-       virtual int check_if_to_abort()=0;   
-       virtual int check_if_to_abort1()=0;
-       virtual int check_if_to_abort2()=0; 
+protected:
+
+       //play as a sender
+       struct sender s;
+       int* func_sender;
+       vector<int>* arr_indexes;
+       int* sub_group_sender;
+       int Nr_sender;	
+       int* R_sender;
+       block r_sender;	
+       std::vector<std::array<block, 2>>* strings; 
+       BitVector* bitvector;
+       block seed_sub_group;
+       crypto* crypt;
+	   
+       //play as a receiver
+       struct receiver recv;
+       int* func_recv;	
+       std::vector<block>* strings_choices;	
+       block r_recv;	
+       int Nc_recv;	  
+       int* sub_group_recv;
+       int Nr_recv;
+       std::vector <int>* R_recv;
+	   
+public:
+	
+       //common attributes
+       static int Nbf;
+       static int Nc_sender; 
+       static Bitstring* BF; 
+       static std::vector<int>* items;
+       static int Not;
+       static int maxOnes;
+       static int items_size;
+       static int seeds_num;
+
+       //c'tor       
+       Party(int numOfOnes,uint32_t m_nSecParam,uint8_t* constSeed );
        
-       virtual unsigned long write_as_a_receiver(void* msg,unsigned long size)=0;
-       virtual unsigned long read_as_a_receiver(void* msg,unsigned long size)=0; 
-       virtual unsigned long write_as_a_sender(void* msg,unsigned long size)=0;
-       virtual unsigned long read_as_a_sender(void* msg,unsigned long size)=0;
+       //setter	
+       static void set(vector<int>* items,int items_size,int Nbf,int n,int Nc,int string_length,int maxOnes,int seeds_num);
 
-       virtual void get_zeros_ones()=0;
-       virtual void arrange_the_indexes()=0;
+       //getters
+       int getNot() const;
+       int getSeedsNum() const;
+       crypto* getCrypto(); 
+       BitVector* getChoices() const;
+       std::vector<block>& getStringsReceiver();
+       std::vector<std::array<block, 2>>& getStrings();
 
-       virtual BitVector* getChoices() const=0;
-       virtual std::vector<block>& getStringsReceiver()=0;
-       virtual  std::vector<std::array<block, 2>>& getStrings()=0;
-       virtual int getK()=0;
-       virtual block* get_RBF_recv() const=0;
-       virtual ~Party(){};
-       virtual int getNot() const=0;
+       //openning sockets with the other players
+       static void init_sender(struct sender* s, int port);
+       static void init_receiver(struct receiver* recv,const char* ip,int port);
+       void init_sender(int port); 
+       void init_receiver(const char* ip,int port); 
+
+       //writing/reading to/from the other player
+       unsigned long write_as_a_receiver(void* msg,unsigned long size);
+       unsigned long read_as_a_receiver(void* msg,unsigned long size); 
+       unsigned long write_as_a_sender(void* msg,unsigned long size);
+       unsigned long read_as_a_sender(void* msg,unsigned long size);  
+
+       //writing/reading to/from socket
+       static unsigned long write_as_a_sender(sender* snd,void* msg,unsigned long size);
+       static unsigned long write_as_a_receiver(receiver* recv,void* msg,unsigned long size);
+       static unsigned long read_as_a_receiver(receiver* recv,void* msg,unsigned long size);
+       static unsigned long read_as_a_sender(sender* snd,void* msg,unsigned long size);
+	
+       //setting and sending/receiving the sub group
+       unsigned long send_sub_group();  
+       void set_sub_group();  
+       unsigned long recv_C(); 
+
+       //computing/receiving R/r
+       void compute_R_r();
+       unsigned long send_R_r();
+       unsigned long recv_R();  
+
+       //creating BF
+       static void create_BF_threads(std::set<int>* h_kokhav,unsigned int* hash_seeds, block seed);
+	
+       //creating the function
+       void get_zeros_ones();
+       void arrange_the_indexes();
+	   
+       //sending/receiving the function
+       unsigned long recv_func();
+       unsigned long send_func(); 
+	
+       //xoring the RBF with the synchGBF
+       void xor_RBF_sender(synchGBF* r); 
+       void xor_RBF_receiver(synchGBF* r);
+	
+       //deleting BF
+       static void delete_bf();
+
+       //aborting's terms
+       int check_if_to_abort();   
+       int check_if_to_abort1();
+       int check_if_to_abort2(); 
+	
+       //d'tor
+       virtual ~Party();
 	   
 };
 	
