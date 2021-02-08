03e95bddda2741a28795682a83c831298f3161a8
NoaHadad <34161861+NoaHadad@users.noreply.github.com>
2021-02-08 11:13:35 +0200

    Update P0.cpp

--- simple/src/P0.cpp
+++ simple/src/P0.cpp
@@ -2,6 +2,6 @@
 
 P0::P0(int port,int numOfOnes,uint32_t m_nSecParam,uint8_t* constSeed ):Party(numOfOnes,m_nSecParam,constSeed)
 {
-     //initializing the sender's socket
+     //initializing the socket as a sender
      init_sender(port);
 }
