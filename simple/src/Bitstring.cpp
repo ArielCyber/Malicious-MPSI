b59ad7465b19e32f19d1c023e880921cbddaa2c6
NoaHadad <34161861+NoaHadad@users.noreply.github.com>
2021-02-14 14:58:45 +0200

    Update Bitstring.cpp

--- simple/src/Bitstring.cpp
+++ simple/src/Bitstring.cpp
@@ -2,20 +2,16 @@
 
 Bitstring::Bitstring(int length, int numOfOnes,crypto* c):length(length){
 	
-    //std::cout<<"creating arr"<<bytes<<std::endl;
     arr=new BYTE[length];
     for (int i=0;i<length;i++) arr[i]=0;
 	
     if (numOfOnes!=0){
 
-        //auto start = std::chrono::steady_clock::now();
-        //std::cout<<"creating indexes"<<bytes<<std::endl;	
 	int* indexes=new int[length];
 	for (int i=0;i<length;i++) indexes[i]=i;
 	int counter=0;
 	int n=numOfOnes;
 	
-	//std::cout<<"creating random vals"<<bytes<<std::endl;	
 	uint64_t* r=new uint64_t[n];
 	c->gen_rnd((BYTE*)r,8*n);
 	while (n){
@@ -32,27 +28,17 @@ Bitstring::Bitstring(int length, int numOfOnes,crypto* c):length(length){
 	for (int i=0;i<numOfOnes;i++)
               set_bit(indexes[i],1);
         delete [] indexes;
-        //std::cout<<"first 100 bits: ";
-        //for (int i=0;i<100;i++) std::cout<<(int)arr[i]<<" ";
-        //std::cout<<std::endl;
-        //auto end = std::chrono::steady_clock::now();
-        //std::cout<<"Random Bitstring generation time = "<<std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()<<std::endl;	
-	//int sum=0;
-        //for (int i=0;i<length;i++) sum+=get_bit(i);
-        //std::cout<<"sum in Bitsring: "<<sum<<", should be? "<<numOfOnes <<std::endl;
-        //sum=0;
-        //for (int i=0;i<length/2;i++) sum+=get_bit(i);
-        //std::cout<<"sum in first half of Bitsring: "<<sum<<", should be~? "<<numOfOnes/2 <<std::endl;
-
   }
      
 
 }
 
+
 Bitstring::Bitstring(int length, BYTE* arr):length(length){
 	this->arr=new BYTE[length];
 	for(int i=0;i<length;i++) this->arr[i]=arr[i];
 }
+
 
 /**
  * replacing the bit at the requested place 
@@ -66,6 +52,7 @@ void Bitstring::set_bit(int place, int new_val){
 
 }
 
+
 /**
  * searching for the bit at the requested place
  *
@@ -77,15 +64,18 @@ int Bitstring::get_bit(int place) const{
     return arr[place];
 }
 
+
 int Bitstring::get_length() const{ return length;}
 
 BYTE* Bitstring::getArr(){
      return arr;
 }
 
+
 Bitstring::~Bitstring(){
      if (arr!=nullptr) delete [] arr;
 }
+
 
 std::ostream& operator<<(std::ostream& os,const Bitstring& b){
      for (int i=0;i<b.length;i++) os<<b.get_bit(i);
